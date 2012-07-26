90bff3cf26201f66fee0a143ca205cc660a7a6ed
Ryan <sokercap@gmail.com>
2012-07-26 17:14:32 -0500

    fixed some permission settings

--- django/ryang/settings.py
+++ django/ryang/settings.py
@@ -14,19 +14,19 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         #'NAME': '/var/www/home/ryang/django/ryang.db',                      # Or path to database file if using sqlite3.
         'NAME': 'e:\\dev\\django\\ryanguthrie\\django\\ryang.db',
-        'USER': 'ryang',                      # Not used with sqlite3.
-        'PASSWORD': 'guthrie',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        #'USER': 'ryang',                      # Not used with sqlite3.
+        #'PASSWORD': 'guthrie',                  # Not used with sqlite3.
+        #'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        #'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
 ##EMAIL CONFIGURATION
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ryan@ryanguthrie.com'
-EMAIL_HOST_PASSWORD = 'praf9suf'
-EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_HOST_USER = 'ryan@ryanguthrie.com'
+#EMAIL_HOST_PASSWORD = ''
+#EMAIL_PORT = 587
 
 
 
